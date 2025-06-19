34931df7aafb2332283234e4237edf287c2fe6e8
Divyansh <yadavdivyansh445@gmail.com>
2025-06-19 07:58:02 +0530

    BubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,4,2};
+        BubbleSort_(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void BubbleSort_(int[] arr){
+        boolean swapped;
+        swapped = false;
+       // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                // swap if the item is smaller than the previous item
+                if(arr[j]<arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+
+            }
+            // if you did not swap for a particular value of i, it means the
+            // array is sorted hence stop the program
+            if(!swapped){
+                break;
+            }
+
+        }
+    }
+}
